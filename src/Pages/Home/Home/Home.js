f3fecaae16046a17337502b0d3ce5ca18c2ef793
mohammedtakbir <takbirhossain678@gmail.com>
2023-01-16 22:22:30 +0600

    speakers seciton added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import FavoriteEvent from '../FavoriteEvent/FavoriteEvent';
+import Speakers from '../Speakers/Speakers';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <FavoriteEvent />
+            <Speakers />
         </div>
     );
 };
