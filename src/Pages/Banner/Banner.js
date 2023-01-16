bc02fa4b4dbc76e8d6972f217d22ae88d53ac639
mohammedtakbir <takbirhossain678@gmail.com>
2023-01-16 21:15:12 +0600

    banner added

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './banner.css';
 
 const Banner = () => {
+
     return (
-        <div>
-            <h2>Banner</h2>
+        <div className='banner flex items-center justify-center'>
+            <div className='text-center text-white'>
+                <h1 className='font-bold text-8xl'>Crystal Events</h1>
+                <h2 className='font-bold text-6xl my-2'>The content strategy Events</h2>
+                <h3 className='font-bold text-2xl mb-5'>Minneapolis, MN • April 30–May 3, 2023</h3>
+                <button className='font-semibold bg-green-500 px-6 py-3 text-2xl rounded-md hover:scale-105 duration-200 inline-block text-white'>Register Now</button>
+            </div>
         </div>
     );
 };
